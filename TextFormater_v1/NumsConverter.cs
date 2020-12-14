ee065020c493764402343336fd26acc17c13aa14
AbdulovHell <dronovova@gmail.com>
2020-12-14 17:53:17 +0300

    конвертация чисел

---
+++ TextFormater_v1/NumsConverter.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TextFormater_v1
+{
+    class NumsConverter
+    {
+        public NumsConverter()
+        {
+
+        }
+
+        public string Convert(string text_in)
+        {
+            string text_out = text_in;
+            Match match;
+            while ((match = Regex.Match(text_out, "[0-9]{1,}", RegexOptions.Compiled)).Success)
+            {
+                int num = int.Parse(match.Value);
+                string text_r = "";
+                if (num <= 9)
+                {
+                    text_r = ConvertDigit(num);
+                }
+                else if (num <= 99)
+                {
+                    text_r = ConvertDozens(num);
+                }
+                else if (num <= 999)
+                {
+                    text_r = ConvertHundreds(num);
+                }else if (num <= 9999)
+                {
+                    text_r = ConvertThousands(num);
+                }
+
+                text_out = text_out.Remove(match.Index, match.Length);
+                text_out = text_out.Insert(match.Index, text_r);
+            }
+
+            return text_out;
+        }
+
+        string Convert(int num)
+        {
+            if (num == 0) return "";
+            if (num <= 9)
+            {
+                return ConvertDigit(num);
+            }
+            else if (num <= 99)
+            {
+                return ConvertDozens(num);
+            }
+            else if (num <= 999)
+            {
+                return ConvertHundreds(num);
+            }
+            else if (num <= 9999)
+            {
+                return ConvertThousands(num);
+            }
+            else
+                return "";
+        }
+
+        string ConvertDigit(int d)
+        {
+            switch (d)
+            {
+                case 0:
+                    return "НОЛЬ";
+                case 1:
+                    return "ОДИН";
+                case 2:
+                    return "ДВА";
+                case 3:
+                    return "ТРИ";
+                case 4:
+                    return "ЧЕТЫРЕ";
+                case 5:
+                    return "ПЯТЬ";
+                case 6:
+                    return "ШЕСТЬ";
+                case 7:
+                    return "СЕМЬ";
+                case 8:
+                    return "ВОСЕМЬ";
+                case 9:
+                    return "ДЕВЯТЬ";
+                default:
+                    return "";
+            }
+        }
+
+        string ConvertDozens(int d)
+        {
+            string temp_d = d.ToString();
+
+            if (temp_d[1] == '0')
+            {
+                switch (temp_d[0])
+                {
+                    case '1':
+                        return "ДЕСЯТЬ";
+                    case '2':
+                        return "ДВАДЦАТЬ";
+                    case '3':
+                        return "ТРИДЦАТЬ";
+                    case '4':
+                        return "СОРОК";
+                    case '5':
+                        return "ПЯТЬДЕСЯТ";
+                    case '6':
+                        return "ШЕСТЬДЕСЯТ";
+                    case '7':
+                        return "СЕМЬДЕСЯТ";
+                    case '8':
+                        return "ВОСЕМЬДЕСЯТ";
+                    case '9':
+                        return "ДЕВЯНОСТО";
+                    default:
+                        return "";
+                }
+            }
+            else if (temp_d[0] == '1')
+            {
+                switch (temp_d[1])
+                {
+                    case '1':
+                        return "ОДИНАДЦАТЬ";
+                    case '2':
+                        return "ДВЕНАДЦАТЬ";
+                    case '3':
+                        return "ТРИНАДЦАТЬ";
+                    case '4':
+                        return "ЧЕТЫРНАДЦАТЬ";
+                    case '5':
+                        return "ПЯТНАДЦАТЬ";
+                    case '6':
+                        return "ШЕСТНАДЦАТЬ";
+                    case '7':
+                        return "СЕМНАДЦАТЬ";
+                    case '8':
+                        return "ВОСЕМНАДЦАТЬ";
+                    case '9':
+                        return "ДЕВЯТНАДЦАТЬ";
+                    default:
+                        return "";
+                }
+            }
+            else
+            {
+                return ConvertDozens((temp_d[0] - 48) * 10) + " " + ConvertDigit(temp_d[1] - 48);
+            }
+        }
+
+        Dictionary<char, string> Hundreds = new Dictionary<char, string>(){
+            {'1',"СТО" },
+            {'2',"ДВЕСТИ" },
+            {'3',"ТРИСТА" },
+            {'4',"ЧЕТЫРЕСТА" },
+            {'5',"ПЯТЬСОТ" },
+            {'6',"ШЕСТЬСОТ" },
+            {'7',"СЕМЬСОТ" },
+            {'8',"ВОСЕМЬСОТ" },
+            {'9',"ДЕВЯТЬСОТ" }
+        };
+
+        string ConvertHundreds(int d)
+        {
+            string temp_d = d.ToString();
+
+            return Hundreds[temp_d[0]] + " " + Convert(d%100);
+        }
+
+        Dictionary<char, string> Thousands = new Dictionary<char, string>()
+        {
+            {'1',"ТЫСЯЧА" },
+            {'2',"ДВЕ ТЫСЯЧИ" },
+            {'3',"ТРИ ТЫСЯЧИ" },
+            {'4',"ЧЕТЫРЕ ТЫСЯЧИ" },
+            {'5',"ПЯТЬ ТЫСЯЧЬ" },
+            {'6',"ШЕСТЬ ТЫСЯЧЬ" },
+            {'7',"СЕМЬ ТЫСЯЧЬ" },
+            {'8',"ВОСЕМЬ ТЫСЯЧЬ" },
+            {'9',"ДЕВЯТЬ ТЫСЯЧЬ" }
+        };
+
+        string ConvertThousands(int d)
+        {
+            string temp_d = d.ToString();
+
+            return Thousands[temp_d[0]] + " " + Convert(d%1000); 
+        }
+    }
+}
--- TextFormater_v1/Program.cs
+++ TextFormater_v1/Program.cs
@@ -10,23 +10,34 @@ namespace TextFormater_v1
     {
         static string FormatText(string input_text)
         {
+            NumsConverter numsConverter = new NumsConverter();
+
             input_text = Regex.Replace(input_text, "!{1,}\"", " "); //замена !" на пробел
             input_text = Regex.Replace(input_text, "\\?!", "."); //замена ?! на .
             //замена пунктуацию на пробелы и .
-            input_text = input_text.Replace('?', '.').Replace('!', '.').Replace('\"', ' ').Replace('(', ' ').Replace(')', ' ').Replace(',', ' ').Replace('-', ' ').Replace(':', ' '); 
-            input_text = input_text.ToUpper().Replace('.','\n'); //переход в верхний регистр и замена точек на \n
-            input_text = new string(input_text.Where(c => !char.IsDigit(c)).Where(c => c != '\r').ToArray()); //отфильтровываем цифры и \r
+            input_text = input_text.Replace('?', '.').Replace('!', '.').Replace('\"', ' ').Replace('(', ' ').Replace(')', ' ').Replace(',', ' ').Replace('-', ' ').Replace(':', ' ');
+            input_text = input_text.ToUpper().Replace('.', '\n'); //переход в верхний регистр и замена точек на \n
+            input_text = numsConverter.Convert(input_text); //преобразование чисел 0-9999 в текстовый вид
+            input_text = new string(input_text.Where(c => !char.IsDigit(c)).Where(c => c != '\r').ToArray()); //отфильтровываем оставшиеся цифры и \r
             input_text = Regex.Replace(input_text, "\\u0020{2,}", " "); //если идут несколько пробелов подряд, оставляем один
             input_text = Regex.Replace(input_text, "\\n\\u0020", "\n"); //если перед началом предложения пробел, убираем пробел
             input_text = Regex.Replace(input_text, "\\n{2,}", "\n"); //если идут несколько \n подряд, оставляем один
 
-            return input_text; 
+            return input_text;
+        }
+
+        static string AddPrefix(string path)
+        {
+            int offset = path.Reverse().TakeWhile(c => c != '.').Count() + 1;
+            return path.Insert(path.Length - offset, "_formated");
         }
 
         static void Main(string[] args)
-        {            
-            //string txt=File.ReadAllText("D:\\test_text.txt");
-            //File.WriteAllText("D:\\test_text_formated.txt", FormatText(txt));
+        {
+            //var test=AddPrefix("dummy_file.12.txt");
+
+            //string txt=File.ReadAllText("D:\\nums.txt");
+            //File.WriteAllText("D:\\nums_formated.txt", FormatText(txt));
 
             if (args.Length == 0) return;
 
@@ -35,7 +46,7 @@ namespace TextFormater_v1
                 Console.WriteLine("Обработка {0}", item);
                 var text = File.ReadAllText(item);
                 var output_text = FormatText(text);
-                File.WriteAllText(item + "_f", output_text);
+                File.WriteAllText(AddPrefix(item), output_text);
             }
 
             Console.WriteLine("Обработано {0} файлов.", args.Length);
